2b213e83371c776915aee1fad1922332f2794c00
Glitch (hello-sqlite) <none>
2021-01-21 13:35:08 +0000

    🌋🎁 Checkpoint ./server.js:27667977/410

--- server.js
+++ server.js
@@ -5,6 +5,16 @@ client.on("message", message => {
 if(message.content === "ping") {
   message.channel.send("pong")
 }
-})
+if(message.content === "embed") {
+  let embed = new Discord.MessageEmbed()
+  .setTitle("This is Embed Title")
+  .setDescription("This Is Embed description")
+  .setcolor("RANDOM")
+  .setFooter("This is Embed Footer");
+  message.channel.send(embed);
+ }
+});
+
+
 
 client.login("ODAxNzk3NTMyMTExNzMyNzU2.YAl6JA.oMpiAQDsN5yb0M8hLUe6-kkbIXA")
